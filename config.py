02a57d4046b157a7f7ee7dc115c20443d59f1b17
Jacobia Johnson <johnsonjacobia@gmail.com>
2020-09-08 01:50:15 -0400

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ load_dotenv(os.path.join(basedir, '.env'))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or os.environ.get('CLEARDB_DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     MAIL_SERVER=os.environ.get('MAIL_SERVER')
