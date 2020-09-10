49c37b6705730226df5ad3024e4bdb908fcbdf02
Jacobia Johnson <johnsonjacobia@gmail.com>
2020-09-10 15:43:30 -0400

    adding and editing admin and zones

--- app/admin/forms.py
+++ app/admin/forms.py
@@ -1,7 +1,7 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, SelectField, HiddenField
 from wtforms.validators import DataRequired,Email, ValidationError
-from app.models import User
+from app.models import User, Zone
 
 
 def validate_name(form,_):
@@ -14,6 +14,7 @@ class AddAdminForm(FlaskForm):
     first_name = StringField('First Name',validators=[DataRequired(),validate_name])
     last_name = StringField('Last Name', validators=[DataRequired(), validate_name])
     middle_initial = StringField('Middle Initial', validators=[DataRequired(), validate_name])
+    group = SelectField(u'Admin Type', coerce=int, validators=[DataRequired()])
     submit = SubmitField('Add Aministrator')
 
     def validate_email(self, email):
@@ -22,8 +23,43 @@ class AddAdminForm(FlaskForm):
             raise ValidationError('Please use a different email address.')
 
 class EditAdminForm(FlaskForm):
+    #BUG an error occurs if the edited information is unique to another row in the table
+    # this should be addressed by checking to see if there are any users besides the one
+    # being updated with the same information
     email = StringField('Email', validators=[DataRequired(), Email()])
     first_name = StringField('First Name',validators=[DataRequired()])
     last_name = StringField('Last Name', validators=[DataRequired()])
     middle_initial = StringField('Middle Initial', validators=[DataRequired()])
+    group = SelectField(u'Admin Type', coerce=int, validators=[DataRequired()])
     submit = SubmitField('Edit Aministrator')
+
+class AddZoneForm(FlaskForm):
+    name = StringField('Zone Name', validators=[DataRequired()])
+    color = StringField('Zone Color',validators=[DataRequired()])
+    submit = SubmitField('Add Zone')
+    
+    def validate_name(self, name):
+        zone = Zone.query.filter_by(name=name.data).first()
+        if zone is not None:
+            raise ValidationError('Please use a unique zone name.')
+
+    def validate_color(self, color):
+        color = Zone.query.filter_by(color=color.data).first()
+        if color is not None:
+            raise ValidationError('Please use a unique zone color.')
+
+class EditZoneForm(FlaskForm):
+    zone_id = HiddenField()
+    name = StringField('Zone Name', validators=[DataRequired()])
+    color = StringField('Zone Color',validators=[DataRequired()])
+    submit = SubmitField('Edit Zone')
+    
+    def validate_name(self, name):
+        zone = Zone.query.filter_by(name=name.data).filter(id != self.zone_id).first()
+        if zone is not None:
+            raise ValidationError('Please use a unique zone name.')
+
+    def validate_color(self, color):
+        color = Zone.query.filter_by(color=color.data).filter(id != self.zone_id).first()
+        if color is not None:
+            raise ValidationError('Please use a unique zone color.')
--- app/admin/routes.py
+++ app/admin/routes.py
@@ -1,8 +1,8 @@
 from app.admin import bp
 from flask import render_template, url_for, flash, redirect
 from flask_login import login_required
-from app.models import User, Zone, Camera, ParkingSpace, Lot, SystemLog
-from app.admin.forms import AddAdminForm, EditAdminForm
+from app.models import User, Zone, Camera, ParkingSpace, Lot, SystemLog, AdminGroup
+from app.admin.forms import AddAdminForm, EditAdminForm, AddZoneForm, EditZoneForm
 from app.email import send_activation_email
 from app import db
 
@@ -25,11 +25,12 @@ def administrators():
 @login_required
 def add_administrator():
     form = AddAdminForm()
+    form.group.choices = [(g.id, g.name) for g in AdminGroup.query.order_by('name')]
+
     if form.validate_on_submit():
         # form.validate_name(form.first_name.data, form.last_name.data,form.middle_initial.data)
         try:
-            user = User( email=form.email.data, first_name = form.first_name.data,last_name = form.last_name.data,middle_initial = form.middle_initial.data)
-            #TODO send activiation message to the new user
+            user = User( email=form.email.data, first_name = form.first_name.data,last_name = form.last_name.data,middle_initial = form.middle_initial.data, group_id=form.group.data)
 
             db.session.add(user)
             db.session.commit()
@@ -46,6 +47,8 @@ def add_administrator():
 @login_required
 def edit_administrator(user_id):
     form = EditAdminForm()
+    form.group.choices = [(g.id, g.name) for g in AdminGroup.query.order_by('name')]
+
     if form.validate_on_submit():
         try:
             admin = User.query.get(user_id)
@@ -53,7 +56,9 @@ def edit_administrator(user_id):
             admin.first_name = form.first_name.data
             admin.last_name = form.last_name.data
             admin.middle_initial = form.middle_initial.data
+            admin.group_id = form.group.data
             db.session.commit()
+            flash('Administrator updated successfully!')
         except Exception as error:
             print(error)
             flash("Something went wrong! Try again later")
@@ -62,6 +67,7 @@ def edit_administrator(user_id):
         return redirect(url_for('admin.administrators'))
     
     admin = User.query.get(user_id)
+    form.group.data = admin.group_id
     return render_template("admin/administrators/edit_administrator.html", title='Administrators', form=form, admin=admin)
 
 
@@ -110,12 +116,43 @@ def zones():
 @bp.route('/zones/add', methods=['GET', 'POST'])
 @login_required
 def add_zone():
-    return render_template("admin/zones/zones.html", title='Zones')
+    form = AddZoneForm()
 
-@bp.route('/zones/edit',  methods=['GET', 'POST'])
+    if form.validate_on_submit():
+        try:
+            zone = Zone(name=form.name.data, color=form.color.data)
+
+            db.session.add(zone)
+            db.session.commit()
+            flash("New Zone Added")
+            return redirect(url_for('admin.zones'))
+        except Exception as err:
+            print(err)
+            flash("Something went wrong! Try again later")
+            return render_template("admin/zones/add_zones.html", title='Add Zone', form=form, error=1)
+    return render_template("admin/zones/add_zones.html", title='Add Zone', form=form)
+
+@bp.route('/zones/edit/<zone_id>',  methods=['GET', 'POST'])
 @login_required
-def edit_zone():
-    return render_template("admin/zones/zones.html", title='Zones')
+def edit_zone(zone_id):
+    form = EditZoneForm(zone_id=zone_id)
+
+    if form.validate_on_submit():
+        try:
+            zone = Zone.query.get(zone_id)
+            zone.name = form.name.data
+            zone.color = form.color.data
+            db.session.commit()
+            flash('Zone updated successfully!')
+        except Exception as error:
+            print(error)
+            flash("Something went wrong! Try again later")
+            return redirect("admin/zones/edit_zones.html", title='Edit Zone', form=form, zone=Zone.query.get(zone_id), error=1)
+
+        return render_template(url_for('admin.zones'))
+    
+    zone = Zone.query.get(zone_id)
+    return render_template("admin/zones/edit_zone.html", title='Edit Zone', form=form)
 
 
 @bp.route('/zones/delete',  methods=['POST'])
