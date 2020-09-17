a9c6cef472e2b243e4d1fc911d580b77c1bc93f5
Jacobia Johnson <johnsonjacobia@gmail.com>
2020-09-17 18:30:12 -0400

    updating migrations

---
+++ migrations/versions/33361fefcf42_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 33361fefcf42
+Revises: 
+Create Date: 2020-09-17 18:22:57.821758
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '33361fefcf42'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
