ca0fb995139a36b40c7d755b1772da21d65ea283
Jacobia Johnson <johnsonjacobia@gmail.com>
2020-10-24 11:32:53 -0400

    changing the way error emails are sent to allow for more customized templates

--- app/__init__.py
+++ app/__init__.py
@@ -65,22 +65,6 @@ def create_app(config_class=None):
     app.register_blueprint(admin_bp, url_prefix='/admin')
 
     if not app.debug and not app.testing:
-        if app.config['MAIL_SERVER']:
-            auth = None
-            if app.config['MAIL_USERNAME'] or app.config['MAIL_PASSWORD']:
-                auth = (app.config['MAIL_USERNAME'],
-                        app.config['MAIL_PASSWORD'])
-            secure = None
-            if app.config['MAIL_USE_TLS']:
-                secure = ()
-            mail_handler = SMTPHandler(
-                mailhost=(app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
-                fromaddr=app.config['ADMIN'],
-                toaddrs=app.config['ADMIN'], subject='[Soar High Parking] System Failure',
-                credentials=auth, secure=secure, timeout=10.0)
-            mail_handler.setLevel(logging.ERROR)
-            app.logger.addHandler(mail_handler)
-
         if app.config['LOG_TO_STDOUT']:
             stream_handler = logging.StreamHandler()
             stream_handler.setLevel(logging.INFO)
--- app/errors/handlers.py
+++ app/errors/handlers.py
@@ -1,8 +1,10 @@
 """This module holds all error handlers."""
-from flask import render_template
+import traceback
+from flask import render_template, current_app
 
 from app import db
 from app.errors import bp
+from app.email import send_email
 
 
 @bp.app_errorhandler(404)
@@ -22,5 +24,11 @@ def internal_error(error):
 
     :param error: The error that triggered the handler.
     """
+    error_tb = traceback.format_exc()
+    send_email('[Soar High Parking] System Failure',
+               sender=current_app.config['ADMIN'],
+               recipients=current_app.config['ADMIN'],
+               html_body=render_template('error_message_email.html',
+                                         error=error_tb))
     db.session.rollback()
     return render_template('500.html'), 500
